67e99dad0557bc30e946465b10d4766e04dbc333
Amr M. Kayid <amrkayid2027@gmail.com>
2019-03-12 09:32:32 +0100

    Utils data conv

--- kayddrl/utils/utils.py
+++ kayddrl/utils/utils.py
@@ -1,6 +1,9 @@
 import torch
+import random
 import numpy as np
 import pydash as ps
+
+from pprint import pformat
 
 
 # -------------------- Seeding -------------------- #
@@ -9,6 +12,7 @@ def set_global_seeds(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+
 
 # -------------------- ---------- -------------------- #
 
@@ -53,5 +57,30 @@ def describe(cls):
         desc_list.append(f'- {k} = {desc_v}')
     desc = '\n'.join(desc_list)
     return desc
+
+
+# -------------------- ---------- -------------------- #
+
+# -------------------- Data Types -------------------- #
+
+
+def numpify(x, dtype):
+    if torch.is_tensor(x):
+        return x.detach().cpu().numpy().astype(dtype)
+    elif isinstance(x, np.ndarray):
+        return x.astype(dtype)
+    else:
+        return np.asarray(x, dtype=dtype)
+
+
+def tensorify(x, device):
+    if torch.is_tensor(x):
+        if str(x.device) != str(device):
+            x = x.to(device)
+        return x
+    elif isinstance(x, np.ndarray):
+        return torch.from_numpy(x).float().to(device)
+    else:
+        return torch.from_numpy(np.asarray(x)).float().to(device)
 
 # -------------------- ---------- -------------------- #
